88c11c4840f00579f365b12e45fa9c4f66652a0c
Eugen <chesnok297@gmail.com>
2019-07-19 14:25:58 +0300

    Check connect

--- tcpcontrol.cpp
+++ tcpcontrol.cpp
@@ -8,24 +8,31 @@ TcpControl::TcpControl(QObject *parent) : QObject(parent)
     //this->port = port;
 
     tcpSocket = new QTcpSocket(this);
-    qDebug()<<"start 1";
+    //qDebug()<<"start 1";
     connect(tcpSocket, SIGNAL(readyRead()), this, SLOT(socketReady()));
     connect(tcpSocket, SIGNAL(disconnected()), this, SLOT(socketDisconnected()));
 
     tcpSocket->connectToHost("localhost", 7777);
-    qDebug()<<"start 2 conectted";
+    if(!tcpSocket->waitForConnected(5000))
+    {
+        qDebug() << "Error connect: " << tcpSocket->errorString();
+    }
+    else
+    {
+        qDebug()<<"Start client!";
+    }
 }
 
 void TcpControl::socketReady() {
 
-     qDebug() << "ready";
+     //qDebug() << "ready";
     if(tcpSocket->waitForConnected(1000)) {
         tcpSocket->waitForReadyRead(1000);
         //QByteArray *data = new QByteArray();
         //*data = tcpSocket->readAll();
         QByteArray data = tcpSocket->readAll();
 
-        qDebug() << data;
+        qDebug() << data << "Get complete!";
     }
 }
 
